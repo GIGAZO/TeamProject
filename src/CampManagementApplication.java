e7ab9dabe38bfce272098cbbd58caefea4f6b411
hsh1105 <hantg1105@naver.com>
2024-05-07 14:35:07 +0900

    기능구현

--- src/CampManagementApplication.java
+++ src/CampManagementApplication.java
@@ -220,6 +220,10 @@ public class CampManagementApplication {
     // 수강생 목록 조회 (승훈님 파트)
     private static void inquireStudent() {
         System.out.println("\n수강생 목록을 조회합니다...");
+        for(Student student : studentStore) {
+            System.out.println(student.getStudentId());
+            System.out.println(student.getStudentName());
+        }
         // 기능 구현
         System.out.println("\n수강생 목록 조회 성공!");
     }
